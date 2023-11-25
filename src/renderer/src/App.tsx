f0692e53dc4a34356349d38269acc3877eb57ba1
Icaro <iba1987@gmail.com>
2023-11-25 18:31:46 -0300

    feat(titlebar): create component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,11 @@
-const App = (): JSX.Element => {
-  return <h1 className="text-red-500">Hello World</h1>
-}
+import { JSX } from 'react'
+import { Titlebar } from './components/Titlebar'
 
-export default App
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <Titlebar title="Jogabiliday" />
+      <h1 className="text-red-500">Hello World</h1>
+    </>
+  )
+}
---
+++ src/renderer/src/components/Titlebar.tsx
@@ -0,0 +1,31 @@
+import { JSX } from 'react'
+import { LuMinimize2, LuMaximize2, LuX, LuMinus } from 'react-icons/lu'
+
+type Props = {
+  title: string
+  icon?: string
+}
+
+export const Titlebar = ({ title, icon }: Props): JSX.Element => {
+  const isMaximized = false //update to state after
+
+  return (
+    <header className="flex justify-between bg-zinc-800 text-zinc-100">
+      <div className="flex items-center gap-2 px-3">
+        {icon && <span>{icon}</span>}
+        <span>{title}</span>
+      </div>
+      <div className="flex">
+        <button className="flex items-center justify-center w-8 p-1 transition-all duration-150 cursor-default aspect-square bg-zinc-800 hover:bg-zinc-700">
+          <LuMinus />
+        </button>
+        <button className="flex items-center justify-center w-8 p-1 transition-all duration-150 cursor-default aspect-square bg-zinc-800 hover:bg-zinc-700">
+          {isMaximized ? <LuMinimize2 /> : <LuMaximize2 />}
+        </button>
+        <button className="flex items-center justify-center w-8 p-1 transition-all duration-150 cursor-default aspect-square bg-zinc-800 hover:bg-red-600">
+          <LuX />
+        </button>
+      </div>
+    </header>
+  )
+}
