0089407c2811a6d634abb73225df3f814302ac0a
Icaro <iba1987@gmail.com>
2023-11-25 21:29:49 -0300

    feat(main page): add component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,12 @@
 import { JSX } from 'react'
 import { Titlebar } from './components/Titlebar'
+import { Main } from './pages/Main'
 
 export const App = (): JSX.Element => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Titlebar title="Jogabiliday" />
-      <h1 className="text-red-500">Hello World</h1>
-    </>
+      <Main />
+    </div>
   )
 }
---
+++ src/renderer/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react'
+
+export const Main = (): JSX.Element => {
+  return (
+    <main className="flex flex-col justify-between flex-grow p-3 bg-zinc-800 text-zinc-100">
+      <h1>Lorem</h1>
+      <h1>Ipsum</h1>
+    </main>
+  )
+}
