ffc642bd177863a97af8d7c3daa7323af9622635
Icaro <iba1987@gmail.com>
2023-11-27 17:53:46 -0300

    feat(main-page): create accordion component

---
+++ src/renderer/src/components/Accordion.tsx
@@ -0,0 +1,26 @@
+import { JSX, useState } from 'react'
+import { LuChevronDown, LuChevronUp } from 'react-icons/lu'
+
+type Props = {
+  title: string
+  children: JSX.Element
+}
+
+export const Accordion = (props: Props): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <article>
+      <header className="flex justify-between">
+        <h3 className="text-teal-300">{props.title}</h3>
+        <button
+          className="w-8 aspect-square rounded-md flex items-center justify-center hover:text-teal-300 transition-colors bg-inherit hover:bg-zinc-700"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <LuChevronUp /> : <LuChevronDown />}
+        </button>
+      </header>
+      {isOpen && <section className="p-2">{props.children}</section>}
+    </article>
+  )
+}
