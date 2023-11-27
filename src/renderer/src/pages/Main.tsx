673bc989b697901c36d9519079e384470de92eaa
Icaro <iba1987@gmail.com>
2023-11-27 18:11:32 -0300

    feat(main-page): update window and add accordions

--- src/renderer/src/pages/Main.tsx
+++ src/renderer/src/pages/Main.tsx
@@ -1,9 +1,29 @@
+import { Accordion } from '@renderer/components/Accordion'
 import { JSX } from 'react'
 
 export const Main = (): JSX.Element => {
   return (
     <main className="flex flex-col justify-between flex-grow p-3 bg-zinc-800 text-zinc-100">
-      <h1>Lorem</h1>
+      <div className="flex flex-col gap-4">
+        <Accordion title="Sites">
+          <>
+            <h2>Orelo</h2>
+            <div>
+              <button>Abrir janela</button>
+            </div>
+            <h2>Tipa Ai</h2>
+            <div>
+              <button>Abrir janela</button>
+            </div>
+          </>
+        </Accordion>
+        <Accordion title="Metas">
+          <h2>Metas</h2>
+        </Accordion>
+        <Accordion title="Configurações">
+          <h2>Configurações</h2>
+        </Accordion>
+      </div>
       <h1>Ipsum</h1>
     </main>
   )
