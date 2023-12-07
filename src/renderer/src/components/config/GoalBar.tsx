977a044552974b46dbadd8e30a63578e4dae34b1
Icaro <iba1987@gmail.com>
2023-12-07 18:06:14 -0300

    feat(store): persist bar settings on client

--- src/renderer/src/components/config/GoalBar.tsx
+++ src/renderer/src/components/config/GoalBar.tsx
@@ -1,5 +1,6 @@
+import { useGoalBarStore } from '@renderer/store/goalBarStore'
 import { Input } from '../Input'
-import { JSX, useRef, useState } from 'react'
+import { JSX, useEffect, useRef, useState } from 'react'
 
 export const GoalBarConfig = (): JSX.Element => {
   const bgColorRef = useRef<HTMLInputElement>(null)
@@ -12,49 +13,90 @@ export const GoalBarConfig = (): JSX.Element => {
   const textSizeRef = useRef<HTMLInputElement>(null)
   const valueSizeRef = useRef<HTMLInputElement>(null)
   const textWeightRef = useRef<HTMLInputElement>(null)
+  const {
+    bgColor,
+    border,
+    fillColor,
+    height,
+    paddingH,
+    paddingV,
+    textSize,
+    textWeight,
+    updateBgColor,
+    updateBorder,
+    updateFillColor,
+    updateHeight,
+    updatePaddingH,
+    updatePaddingV,
+    updateTextSize,
+    updateTextWeight,
+    updateValueSize,
+    updateWidth,
+    valueSize,
+    width
+  } = useGoalBarStore((state) => ({ ...state }))
   const [timeoutHandler, setTimeoutHandler] = useState<ReturnType<typeof setTimeout> | null>(null)
+
+  useEffect(() => {
+    window.mainApi.customizeBar({
+      bgColor,
+      fillColor,
+      border,
+      height,
+      paddingH,
+      paddingV,
+      textSize,
+      textWeight,
+      valueSize,
+      width
+    })
+  }, [
+    bgColor,
+    fillColor,
+    border,
+    height,
+    paddingH,
+    paddingV,
+    textSize,
+    textWeight,
+    valueSize,
+    width
+  ])
 
   const customizeBar = (): void => {
     if (timeoutHandler) clearTimeout(timeoutHandler)
     const handler = setTimeout(() => {
-      if (
-        bgColorRef.current &&
-        fillColorRef.current &&
-        widthRef.current &&
-        heightRef.current &&
-        borderRef.current &&
-        paddingHRef.current &&
-        paddingVRef.current &&
-        textSizeRef.current &&
-        valueSizeRef.current &&
-        textWeightRef.current
-      ) {
-        const colorRegex = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/
-        if (
-          !bgColorRef.current.value.match(colorRegex) ||
-          !fillColorRef.current.value.match(colorRegex) ||
-          isNaN(+widthRef.current?.value) ||
-          isNaN(+heightRef.current?.value) ||
-          isNaN(+borderRef.current?.value) ||
-          isNaN(+paddingHRef.current?.value) ||
-          isNaN(+paddingVRef.current?.value) ||
-          isNaN(+textSizeRef.current?.value) ||
-          isNaN(+valueSizeRef.current?.value) ||
-          isNaN(+textWeightRef.current?.value)
-        )
-          return
-        window.mainApi.customizeBar({
-          bgColor: bgColorRef.current.value,
-          fillColor: fillColorRef.current.value,
-          border: +borderRef.current.value,
-          height: +heightRef.current.value,
-          paddingH: +paddingHRef.current.value,
-          paddingV: +paddingVRef.current.value,
-          textSize: +textSizeRef.current.value,
-          textWeight: +textWeightRef.current.value,
-          valueSize: +valueSizeRef.current.value,
-          width: +widthRef.current.value
-        })
+      const colorRegex = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/
+
+      if (bgColorRef.current && !bgColorRef.current.value.match(colorRegex)) {
+        updateBgColor(bgColorRef.current.value)
+      }
+      if (fillColorRef.current && !fillColorRef.current.value.match(colorRegex)) {
+        updateFillColor(fillColorRef.current.value)
+      }
+      if (widthRef.current && !isNaN(+widthRef.current.value)) {
+        updateWidth(+widthRef.current.value)
+      }
+      if (heightRef.current && !isNaN(+heightRef.current.value)) {
+        updateHeight(+heightRef.current.value)
+      }
+      if (borderRef.current && !isNaN(+borderRef.current.value)) {
+        updateBorder(+borderRef.current.value)
+      }
+      if (paddingHRef.current && !isNaN(+paddingHRef.current.value)) {
+        updatePaddingH(+paddingHRef.current.value)
+      }
+      if (paddingVRef.current && !isNaN(+paddingVRef.current.value)) {
+        updatePaddingV(+paddingVRef.current.value)
+      }
+      if (textSizeRef.current && !isNaN(+textSizeRef.current.value)) {
+        updateTextSize(+textSizeRef.current.value)
+      }
+      if (valueSizeRef.current && !isNaN(+valueSizeRef.current.value)) {
+        updateValueSize(+valueSizeRef.current.value)
+      }
+      if (textWeightRef.current && !isNaN(+textWeightRef.current.value)) {
+        updateTextWeight(+textWeightRef.current.value)
       }
     }, 700)
     setTimeoutHandler(handler)
@@ -68,19 +110,31 @@ export const GoalBarConfig = (): JSX.Element => {
           label="Cor Primária"
           onChange={customizeBar}
           _ref={fillColorRef}
-          value="#42FFEB"
+          value={fillColor}
         />
         <Input
           type="color"
           label="Cor Secundária"
           onChange={customizeBar}
           _ref={bgColorRef}
-          value="#261752"
+          value={bgColor}
         />
       </div>
       <div className="flex justify-between gap-2">
-        <Input _ref={widthRef} label="Largura" type="text" onChange={customizeBar} value="400" />
-        <Input _ref={heightRef} label="Altura" type="text" onChange={customizeBar} value="60" />
+        <Input
+          _ref={widthRef}
+          label="Largura"
+          type="text"
+          onChange={customizeBar}
+          value={width + ''}
+        />
+        <Input
+          _ref={heightRef}
+          label="Altura"
+          type="text"
+          onChange={customizeBar}
+          value={height + ''}
+        />
       </div>
       <div className="flex justify-between gap-2">
         <Input
@@ -88,14 +142,14 @@ export const GoalBarConfig = (): JSX.Element => {
           label="Espaçamento Horizontal"
           type="text"
           onChange={customizeBar}
-          value="8"
+          value={paddingH + ''}
         />
         <Input
           _ref={paddingVRef}
           label="Espaçamento Vertical"
           type="text"
           onChange={customizeBar}
-          value="8"
+          value={paddingV + ''}
         />
       </div>
       <div className="flex justify-between gap-2">
@@ -104,24 +158,30 @@ export const GoalBarConfig = (): JSX.Element => {
           label="Tamanho texto"
           type="text"
           onChange={customizeBar}
-          value="18"
+          value={textSize + ''}
         />
         <Input
           _ref={valueSizeRef}
           label="Tamanho valor"
           type="text"
           onChange={customizeBar}
-          value="32"
+          value={valueSize + ''}
         />
       </div>
       <div className="flex justify-between gap-2">
-        <Input _ref={borderRef} label="Borda" type="text" onChange={customizeBar} value="4" />
+        <Input
+          _ref={borderRef}
+          label="Borda"
+          type="text"
+          onChange={customizeBar}
+          value={border + ''}
+        />
         <Input
           _ref={textWeightRef}
           label="Peso fonte"
           type="text"
           onChange={customizeBar}
-          value="500"
+          value={textWeight + ''}
         />
       </div>
     </>
